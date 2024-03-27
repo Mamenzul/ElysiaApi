5ffe00433bcf8ff1dd44b35f2e9ee723806caee7
VicoBrets <victor.brettes@gmail.com>
2024-03-27 22:07:38 +0100

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Elysia } from "elysia";
 import swagger from "@elysiajs/swagger";
 import { apiRoutes } from "./api";
+import { env } from "./env";
 
-export const app = new Elysia().use(swagger()).use(apiRoutes).listen(3000);
+export const app = new Elysia().use(swagger()).use(apiRoutes).get('/', async (ctx) => {
+  return {
+    status: 'success',
+    message: 'Welcome to Elysia',
+  };
+})
+.get('/health', (ctx) => 'ok');
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.listen({ port: env.PORT }, ({ hostname, port }) => {
+  const url = env.NODE_ENV === 'production' ? 'https' : 'http';
+  console.log(`🦊 Elysia is running at ${url}://${hostname}:${port}`);
+});
